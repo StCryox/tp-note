5dbafc8ef0472266553cf6fa51ac92871d9f6aaf
Ifzas Nguyen <nguyen.ifzas@gmail.com>
2022-12-13 16:27:05 +0100

    broken but working compteMots

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,16 @@
 object Main extends App {
-  println("Hello, World!")
+
+  println(compteMots("     Hello World"));
+  def compteMots(chaine: String): Int = chaine.trim() match {
+    case "" => 0
+    case _ => { 
+      if(chaine.head.isUpper) {
+          1 + compteMots(chaine.tail)
+      } else {
+          compteMots(chaine.tail);
+          
+      }
+    }
+  }
 }
+
